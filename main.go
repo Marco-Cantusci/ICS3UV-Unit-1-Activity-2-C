f686772c2bcea0ff6d04ee59a688f5010e43816e
default <marco.cantusci@stu.ocsb.ca>
2025-09-21 23:48:56 +0000

    String literal text on Go

---
+++ main.go
@@ -0,0 +1,24 @@
+/**
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-09-21
+ * @fileoverview Practicing string literals.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	// string literal text
+	fmt.Println("I love Computer Science!!")
+
+	//string literal text
+	fmt.Println("    I  love Computer Science!!")
+
+	//string literal text
+	fmt.Println("I   love   Computer   Science   100%")
+
+	fmt.Println("\nDone.")
+}
