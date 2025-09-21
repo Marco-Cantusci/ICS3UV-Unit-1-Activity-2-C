c02aa53792ff67cb5f00c5e65bce9b969e2f7443
default <marco.cantusci@stu.ocsb.ca>
2025-09-21 23:39:41 +0000

    String literals on TypeScript

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-09-21
+ * @fileoverview Practicing String Literals.
+ */
+
+// string literal text
+console.log("I love Computer Science!!");
+
+// string literal text
+console.log("    I  love Computer Science!!");
+
+// string literal text
+console.log("I   love   Computer   Science   100%");
+
+console.log("\nDone.");
